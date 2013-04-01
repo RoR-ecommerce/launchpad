49a14f4bfea6045814e9579e99a52838f7ee987d
Yury Velikanau <yury.velikanau@gmail.com>
2013-03-31 21:07:54 -0700

    Rails 4 friendly finders.

--- app/controllers/oauth/auth_controller.rb
+++ app/controllers/oauth/auth_controller.rb
@@ -2,14 +2,15 @@ class Oauth::AuthController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :client_does_not_exist
 
   def new
-    client = Client.find_by_client_id(params[:client_id])
+    client = Client.where(client_id: params[:client_id]).first!
+
     redirect_to oauth_auth_path(
       client_id: client.client_id, redirect_uri: params[:redirect_uri],
       state: params[:state])
   end
 
   def create
-    client       = Client.find_by_client_id(params[:client_id])
+    client       = Client.where(client_id: params[:client_id]).first!
     authorizator = Authorizator.new(current_user, client).create!
     redirector   = UriRedirector.new(client.redirect_uri, params[:redirect_uri])
 
