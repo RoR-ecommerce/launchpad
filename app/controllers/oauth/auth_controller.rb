b547785250d5ab700e1fc4f3b7ca972d6371fb92
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-02 00:04:58 -0700

    Controller now redirects to correct uri.

--- app/controllers/oauth/auth_controller.rb
+++ app/controllers/oauth/auth_controller.rb
@@ -5,10 +5,6 @@ class Oauth::AuthController < ApplicationController
     client     = Client.find_one!(params[:client_id])
     redirector = UriRedirector.new(client.redirect_uri, params[:redirect_uri])
 
-    # FIXME Bug
-    # redirect_to does not understand additional paramethers when the first one
-    # is string, so we need to create full string manually.
-    # redirect_to(redirector.redirect_uri, state: params[:state])
-    redirect_to("#{redirector.redirect_uri}?state=#{params[:state]}")
+    redirect_to(redirector.uri(state: params[:state]))
   end
 end
