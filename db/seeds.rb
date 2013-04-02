4336eb11f9cb02469b2361c7c104347888566d4e
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-01 21:20:57 -0700

    Bootstrap some development data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env.development?
+  Client.create!(name: 'Tracker', uri: 'http://localhost:3000', redirect_uri: 'http://localhost:3000/oauth/callback')
+end
