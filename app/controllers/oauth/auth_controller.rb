b8071de6ecff59bf1dab5a4833864e4ca9eaa455
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-01 21:25:08 -0700

    Remove extra argument.

--- app/controllers/oauth/auth_controller.rb
+++ app/controllers/oauth/auth_controller.rb
@@ -2,7 +2,7 @@ class Oauth::AuthController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :client_does_not_exist
 
   def auth
-    client     = Client.find_one!(params[:client_id], params[:client_secret])
+    client     = Client.find_one!(params[:client_id])
     redirector = UriRedirector.new(client.redirect_uri, params[:redirect_uri])
 
     # FIXME Bug
