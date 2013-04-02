1de73b8086f1c6eba6969bca72966d0b7ce126b9
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-02 00:04:01 -0700

    UriMatcher logic improvements.

--- app/value_classes/uri_matcher.rb
+++ app/value_classes/uri_matcher.rb
@@ -1,18 +1,18 @@
 class UriMatcher
-  attr_reader :redirect_uri
+  attr_reader :uri
 
-  def initialize(redirect_uri)
-    @redirect_uri = redirect_uri
+  def initialize(uri)
+    @uri = uri
   end
 
   def host
-    URI.parse(redirect_uri).host
+    URI.parse(uri).host
   rescue URI::InvalidURIError
     nil
   end
 
   def port
-    URI.parse(redirect_uri).port
+    URI.parse(uri).port
   rescue URI::InvalidURIError
     nil
   end
