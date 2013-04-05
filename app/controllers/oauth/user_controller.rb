89f13a9f1f01b7b6436b10d653da9728b331dcf3
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-04 22:17:44 -0700

    Render user back.

--- app/controllers/oauth/user_controller.rb
+++ app/controllers/oauth/user_controller.rb
@@ -1,7 +1,7 @@
 class Oauth::UserController < ApplicationController
   def user
     if user = User.authorize_with_token(params[:access_token])
-      render json: user.access_token
+      render json: user
     else
       render json: { message: 'Unauthorized Request' }, status: :unauthorized
     end
