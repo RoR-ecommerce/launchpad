f14d948a055085900bd8ced6396370f0d818209b
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-07 15:04:13 -0700

    Error if app is not found.

--- app/controllers/oauth/auth_controller.rb
+++ app/controllers/oauth/auth_controller.rb
@@ -1,15 +1,19 @@
 class Oauth::AuthController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :authorize!
 
   def auth
-    app        = App.authorize!(params[:client_id])
-    auth       = AuthorizationCreator.new(current_user, app).create!
-    redirector = UriRedirector.new(app.redirect_uri, params[:redirect_uri])
+    auth       = AuthorizationCreator.new(current_user, @app).create!
+    redirector = UriRedirector.new(@app.redirect_uri, params[:redirect_uri])
 
     redirect_to redirector.uri \
       code: auth.code, response_type: 'code', state: params[:state]
   end
 
-  # TODO Error response
-  # Make error response if Client is not found, 404?
+  private
+
+  def authorize!
+    @app = App.authorize!(params[:client_id])
+    unauthorized! unless @app
+  end
 end
--- spec/controllers/oauth/auth_controller_spec.rb
+++ spec/controllers/oauth/auth_controller_spec.rb
@@ -6,19 +6,23 @@ describe Oauth::AuthController do
       sign_in(FactoryGirl.create(:user))
     end
 
-    it 'redirects back to app' do
-      app = double(client_id: 1, client_secret: 'secret', redirect_uri: 'http://google.com')
-      App.should_receive(:authorize!).and_return(app)
+    let(:app) { double(client_id: 1, client_secret: 'secret', redirect_uri: 'http://google.com') }
+    let(:auth_code) { double(code: 'secret_code') }
 
-      auth_code = double(code: 'secret_code')
+    it 'redirects back to app' do
+      App.should_receive(:authorize!).and_return(app)
       AuthorizationCreator.any_instance.should_receive(:create!).and_return(auth_code)
 
       get :auth, client_id: 1, redirect_uri: 'http://google.com/auth', state: 'secret_state'
       expect(response).to redirect_to('http://google.com/auth?code=secret_code&response_type=code&state=secret_state')
     end
 
-    it 'does TBD when app is not found' do
-      pending
+    it 'returns error message if app is not found' do
+      App.should_receive(:authorize!).and_return(nil)
+
+      get :auth
+      expect(response.code).to eq('401')
+      expect(response.body).to eq({ message: 'Unauthorized Request' }.to_json)
     end
   end
 end
