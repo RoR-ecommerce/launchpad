68a0cf9672f1d6431e1b80b449a912c1b2b39b02
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-14 18:00:07 -0700

    Update version to check if Rack::Cache is actually does caching.

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Launchpad
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
 
     # Deploying to Heroku requires this setting to be set.
     config.assets.initialize_on_precompile = false
