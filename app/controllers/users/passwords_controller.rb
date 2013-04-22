0338e4aa009391302573ab89c4fb1ec6eec20b58
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-22 12:42:57 -0700

    Reset password token should be in allowed attributes.

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -3,6 +3,7 @@ class Users::PasswordsController < Devise::PasswordsController
 
   def resource_params
     params.require(:user).permit(
-      :email, :password, :password_confirmation, :remember_me)
+      :email, :password, :password_confirmation, :remember_me,
+      :reset_password_token)
   end
 end
