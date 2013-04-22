c57bb284acb75f5544f60981b965dc19b96618ae
Yury Velikanau <yury.velikanau@gmail.com>
2013-04-21 23:41:11 -0700

    Sidekiq client also needs configuration.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,19 @@
 require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
-  if ENV['REDISCLOUD_URL']
-    config.redis = { url: ENV['REDISCLOUD_URL'], namespace: 'launchpad' }
-  end
+  config.redis = { url: ENV['REDISCLOUD_URL'], namespace: 'launchpad' } \
+    if ENV['REDISCLOUD_URL']
 
   database_url = ENV['DATABASE_URL']
   if database_url
     ENV['DATABASE_URL'] = "#{database_url}?pool=25"
     ActiveRecord::Base.establish_connection
   end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDISCLOUD_URL'], namespace: 'launchpad' } \
+    if ENV['REDISCLOUD_URL']
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
